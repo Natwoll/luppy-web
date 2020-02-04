3bc11fb1ad9afd98c0285c1f16a329a431e3896f
Davi Wollmann <davi@centrium.com.br>
2020-02-04 15:14:38 -0200

    maquinas do firestore db para combobox no cadastro de clientes

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -2,37 +2,17 @@ import { Component } from '@angular/core';
 import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 
-export interface Item { 
-/*  name: string;
-  age: number;*/
-  machineId: number;
-}
-
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
+
 export class Tab2Page {
+  private machines: Observable<any[]>;
 
-  private client: AngularFirestoreDocument<Item>;
-  private machines: Observable<any[]>; //transformar em interface
-
-  private fields = {
-    name: 'teste',
-    age: 0,
-  };
-
-  private machineId: number;
-
-  private clientForm = {name: this.fields.name, age: this.fields.age, machineId: this.machineId};
-
-  constructor(private db: AngularFirestore) {
-    this.machines = db.collection('machines/').valueChanges();
-    this.client = db.doc<Item>('clients/a');
+  constructor(db: AngularFirestore) {
+    this.machines = db.collection('machines').valueChanges();
   }
 
-  private registerClient(){
-      this.client.update({machineId: this.machineId});
-  }
 }
