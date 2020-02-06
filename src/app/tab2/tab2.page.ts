8774688feae72269e867296c328d3c3c74c81655
Davi Wollmann <davi@centrium.com.br>
2020-02-05 22:47:11 -0200

    cadastros de clientes e maquinas integrados ao firebase

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,6 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Query } from '@angular/core';
 import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
+import { Observable, pipe } from 'rxjs';
+
+import {NgForm} from '@angular/forms';
+
+export interface Client {
+  name: string;
+  address: string;
+  street: string;
+  number: number;
+  machine: string;
+}
+
+export interface Machine {
+  brand: string;
+  category: string;
+  capacity: string;
+  kind: string;
+  model: string;
+  location: string;
+}
 
 @Component({
   selector: 'app-tab2',
@@ -13,12 +32,40 @@ export class Tab2Page {
   isClient: boolean;
   isMachine: boolean;
 
+  clientDoc: AngularFirestoreDocument<Client>;
+  clientObservable: Observable<Client>;
+
   machines: Observable<any[]>;
+  machineObservable: Observable<Machine>;
+  machineDocument: AngularFirestoreDocument<Machine>;
+
+  clientForm = {
+    name: '',
+    address: '',
+    street: '',
+    number: 0,
+    machine: '',
+  }
+
+  machineForm = {
+    brand: '',
+    category: '',
+    capacity: '',
+    kind: '',
+    model: '',
+    location: '',
+  };
+  
 
   constructor(db: AngularFirestore) {
     this.machines = db.collection('machines').valueChanges();
-  }
+    
+    this.clientDoc = db.doc<Client>('/clients/cliente2');
+    this.clientObservable = this.clientDoc.valueChanges().pipe(data => {return data});
 
+    this.machineDocument = db.doc<Machine>('/machines/machine2');
+    this.machineObservable = this.machineDocument.valueChanges().pipe(data => {return data});
+  }
 
   registerClient() {
     this.isClient = true;
@@ -29,6 +76,30 @@ export class Tab2Page {
   }
 
   completeRegistration(){
+    if(this.isClient && this.isMachine){
+      console.log('Error: client and machine form are both active! Only one can be used at the time.');
+    } else {
+      if(this.isClient){
+        this.clientDoc.set({
+          name: this.clientForm.name,
+          address: this.clientForm.address,
+          street: this.clientForm.street,
+          number: this.clientForm.number,
+          machine: this.clientForm.machine
+        });
+
+      } else {
+        this.machineDocument.set({
+          brand: this.machineForm.brand,
+          category: this.machineForm.category,
+          capacity: this.machineForm.capacity,
+          kind: this.machineForm.kind,
+          model: this.machineForm.model,
+          location: this.machineForm.location,
+        });
+      }
+    }
+
     this.isClient = false;
     this.isMachine = false;
   }
