7e1601569d29dc5fb1d80f67b85e8878609eef92
DaviWollmann <davi.wollmann.s@gmail.com>
2020-02-09 18:48:46 -0300

    funcoa de pesquisar por codigo de qr para obter dados de maquina especifica

--- src/app/common/functions.ts
+++ src/app/common/functions.ts
@@ -1,20 +1,34 @@
 
-export function getFormatedDay(slash: boolean): string {
-    const today: Date = new Date;
-    let day: string = today.getDate().toString();
-    let month: string = today.getMonth().toString();
-    const year: string = today.getFullYear().toString();
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
 
-    day.length == 1 ?
-    day = '0' + today.getDate() : day = today.getDate().toString()
-    
-    month.length == 1 ?
-    month = '0' + today.getDate() : month = today.getDate().toString()
-    
-    if(slash)
-    {
-      return day + '/' + month + '/' + year
-    } else {
-      return day + '-' + month + '-' + year
-    }
+export function serializeDocument (
+  level:'root'|'client',
+  subLevel?:'machine'|'history',
+  clientName?: string
+): string {
+
+  let db: AngularFirestore;
+  let fbDocument: AngularFirestoreDocument;
+
+  return '0'
+}
+
+
+export function getFormatedDay(slash: boolean): string {
+  const today: Date = new Date;
+  let day: string = today.getDate().toString();
+  let month: string = today.getMonth().toString();
+  const year: string = today.getFullYear().toString();
+
+  day.length == 1 ?
+  day = '0' + today.getDate() : day = today.getDate().toString()
+  
+  month.length == 1 ?
+  month = '0' + today.getDate() : month = today.getDate().toString()
+  
+  if(slash) {
+    return day + '/' + month + '/' + year
+  } else {
+    return day + '-' + month + '-' + year
   }
+}
--- src/app/history/history.page.ts
+++ src/app/history/history.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 import { Client, Machine, History as Hist} from '../common/interfaces.page'
-import { getFormatedDay } from '../common/functions'
+import { getFormatedDay, serializeDocument } from '../common/functions'
 
 @Component({
   selector: 'app-history',
@@ -10,79 +10,63 @@ import { getFormatedDay } from '../common/functions'
   styleUrls: ['history.page.scss']
 })
 
-export class History implements OnInit{
-  //must poit to a path as: <clientName>/[machinesOwned]/<machineName>
-  private key: string = 'cliente1/machinesOwned/maquina1';
-  private client: Observable<Client>;
-  private actualMachine: Observable<Machine>;
-  private machines: Observable<Machine[]>;
-  public history: Observable<Hist[]>
-  public isHistoryRecording: boolean;
-  public isMachineRecording: boolean;
-  public historyContent: string;
+export class History  {
+  //Variaveis expostas externamente
+  public key: string;
+  public client: Observable<Client>;
+  public machine: Observable<Machine>;
+  public history: Observable<Hist[]>;
+
+  public teste: string;
 
   constructor(private db: AngularFirestore) {
-    this.history = db.collection<Hist>('/clients/'+this.key+'/history/')
-    .valueChanges();
   }
 
-  ngOnInit(){
-    this.loadClient();
-    this.loadMachine();
+  private loadClient(docReference: string): void {
+    this.client = this.db.doc<Client>(docReference).valueChanges()
   }
 
-  private loadClient(): void {
-    const fbDocument: AngularFirestoreDocument<Client>
-      = this.db.doc<Client>('/clients/'+this.destructKey().clientName);
-
-    this.client = fbDocument.valueChanges();
+  private loadMachine(docReference: string): void {
+    this.client
+    ? this.machine = this.db.doc<Machine>(docReference).valueChanges()
+    : console.log('Could not load the specified machine: Client ins\'t!');
   }
 
-  private loadMachine(): void {
-    const fbDocument: AngularFirestoreDocument<Machine>
-      = this.db.doc<Machine>('/clients/'+this.key);
-
-    this.actualMachine = fbDocument.valueChanges();
+  private loadHistory(collectionReference: string): void {
+    this.machine
+    ? this.history = this.db.collection<Hist>(collectionReference).valueChanges()
+    : console.log('Could not load any history: Machine ins\'t loaded!');
   }
 
-  private destructKey(): any {
-    return {
-      clientName: this.key.substring(0, this.key.indexOf('/')),
-      machineName: this.key.substring(this.key.lastIndexOf('/')+1
-      ,this.key.length)
-    }
-  }
+  private async loadReferences() {
+    return new Promise((resolve, reject) => {
 
-  public insertHistory() {
-
-    console.log(this.historyContent)
-
-    let historyDocument: AngularFirestoreDocument<Hist>;
-
-    historyDocument = this.db.doc<Hist>('/clients/cliente1/machinesOwned/maquina1/history/'+getFormatedDay(false));
-    historyDocument.set({
-      date: getFormatedDay(true),
-      content: this.historyContent
+      if(this.key !== null || this.key.indexOf('/') === 0) {
+        resolve(
+          this.db.firestore.doc('/qrcodes/' + this.key).get()
+          .then(data => { return data.data() })
+          .catch(
+            err => { 
+              console.log('Could not load the proper references for key: ', this.key, err)
+              return null
+          })
+        );
+        
+      } else { reject('error: the specified code key is empty or contains: /'); }
     });
-
-    this.isHistoryRecording = false;
   }
 
-  public insertMachine(){
-    this.isMachineRecording = false;
+  public loadPageContent() {
+    this.loadReferences()
+    .then(
+      (data: any) => {
+        this.loadClient(data.client)
+        this.loadMachine(data.client + 'machinesOwned' + data.machine)
+        this.loadHistory(data.client + 'machinesOwned' + data.machine + 'history')
+      })
+    .catch(
+      err => {
+        console.log(err)
+    });
   }
-
-  addHistory():void {
-    this.isHistoryRecording = true;
-  }
-
-  addMachine():void {
-    this.isMachineRecording = true;
-  }
-
-  public getClient(): Observable<Client> { return this.client }
-  public getActualMachine(): Observable<Machine> { return this.actualMachine }
-  public getMachines(): Observable<Machine[]> { return this.machines }
-  public getHistory(): Observable<Hist[]> { return this.history }
-  
 }
