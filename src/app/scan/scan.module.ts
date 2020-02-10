cc05ef02e2d89fc09e796fe21afdd9f610aaf988
DaviWollmann <davi.wollmann.s@gmail.com>
2020-02-09 23:23:59 -0300

    processo de impressao e serializacao de qrcodes terminado

--- src/app/scan/scan.module.ts
+++ src/app/scan/scan.module.ts
@@ -6,13 +6,22 @@ import { FormsModule } from '@angular/forms';
 import { Scan } from './scan.page';
 import { NgxQRCodeModule } from 'ngx-qrcode2'
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+
+import { environment } from '../../environments/environment';
+import { environment as prodEnvironment} from '../../environments/environment.prod';
+
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
     RouterModule.forChild([{ path: '', component: Scan }]),
-    NgxQRCodeModule
+    NgxQRCodeModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireModule.initializeApp(prodEnvironment.firebase),
+    AngularFirestoreModule,
   ],
   declarations: [Scan]
 })
--- src/app/scan/scan.page.ts
+++ src/app/scan/scan.page.ts
@@ -1,14 +1,111 @@
 import { Component } from '@angular/core';
+import * as jspdf from 'jspdf'
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-scan',
   templateUrl: 'scan.page.html',
   styleUrls: ['scan.page.scss']
 })
+
 export class Scan {
+  private qrValue: string;
+  private qrQtd: number;
+  private qrcodeList = [];
 
-  public qrData: string = 'valor';
+  constructor(private db: AngularFirestore) {
+  }
 
-  constructor() {}
+  private serializeQrCode(quantity: number) {
+    this.getSerialDocumentIndex()
+    .then(
+      (data: any) => {
+        this.setSerialDocumentIndex(data.initials, data.times+quantity);
+        this.qrValue = data.initials;
+      }
+    ).catch(
+      err => {
+        console.log('Got an error tring to serialize the qrcode data', err)
+    });
+  }
+
+  private async setSerialDocumentIndex(initials: string, times: number) {
+    this.db.firestore.doc('/serial/qrcodes/').set({initials: initials, times: times})
+    .then(
+      () => { return true }
+    ).catch(
+      err => {
+        console.log('Got an error tring to set the qrcode serial document', err)
+    });
+  }
+
+  private async getSerialDocumentIndex() {
+    return this.db.firestore.doc('/serial/qrcodes/').get()
+    .then(data => { return data.data() })
+    .catch(
+      err => {
+        console.log('Got an error tring to get the qrcode serial document', err)
+      }
+    );
+  }
+
+  public generateCodes(): void {
+
+    if(this.qrQtd > 18){
+      alert('É permitido imprimir no máximo 18 QrCode por vez');
+    } else {
+      let iterator = 1;
+      let qtdQrcode = this.qrQtd;
+      this.qrcodeList = [];
+      
+      this.getSerialDocumentIndex()
+      .then((data: any) => {
+        if(this.qrQtd > 0){
+          iterator = data.times
+
+          while(iterator < this.qrQtd + data.times){
+            this.qrcodeList.push({data: data.initials + iterator});
+            iterator++;
+          }
+          this.setSerialDocumentIndex(data.initials, iterator)
+        }
+      }).catch(err => { console.log('error generating qrcodes values', err) })
+    }
+  }
+
+  public printQrCodes(): void {
+    const doc = new jspdf();
+    const images = document.querySelectorAll('img');
+    let positionX = 0;
+    let positionY = 0;
+    let column = 1;
+    let listIterator = 0;
+    let qrcodes = this.qrcodeList;
+  
+    doc.setFontSize(20);
+    doc.text('Lista de QR Codes:', 20, 20);
+    doc.setFontSize(13);
+    images.forEach.call(images, function(image: HTMLImageElement) {
+      switch (column) {
+        case 1:
+          positionX = 20;
+          positionY += 40;
+          break;
+        case 2:
+          positionX += 50;
+          break;
+        case 3:
+          positionX += 50;
+          column = 0;
+          break;
+      } column++;
+      
+      doc.addImage(image, 'JPEG', positionX, positionY, 40, 40);
+      doc.text(qrcodes[listIterator].data, positionX+10, positionY+40);
+      listIterator++;
+    });
+
+    doc.save('QrCodeList.pdf')
+  }
 
 }
